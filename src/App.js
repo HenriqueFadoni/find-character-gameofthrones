6738ea599f6c673f67191413f429f73e231c85ea
Henrique Fadoni <henrique@feltapp.com>
2021-01-14 15:29:04 -0300

    Sort By Book & Alphabet

--- src/App.js
+++ src/App.js
@@ -7,24 +7,44 @@ function App() {
 
   const getData = async () => {
     const { data } = await axios.get('https://www.anapioficeandfire.com/api/characters');
-    setCharacters(data);
+    setCharacters([...data]);
   }
 
   useEffect(() => {
     getData();
   }, []);
 
+  const alphabeticOrder = () => {
+    const newCharacters = characters.sort((charA, charB) => {
+      if (charA.aliases < charB.aliases) return -1
+      if (charA.aliases > charB.aliases) return 1
+      return 0;
+    });
+
+    setCharacters([...newCharacters]);
+  }
+
+  const bookOrder = () => {
+    const newCharacters = characters.sort((charA, charB) => {
+      if (charA.books[0] < charB.books[0]) return -1
+      if (charA.books[0] > charB.books[0]) return 1
+      return 0;
+    });
+
+    setCharacters([...newCharacters]);
+  }
+
+  let renderList = characters.map((character, i) => (
+      <p key={i}>{character.aliases[0]}</p>
+    )
+  )
+
   return (
     <div className="App">
       <h1>Find Your Favorite Game of Thrones Characters</h1>
-      <button>Alphabetic Order</button>
-      {
-        characters.map(
-          character => (
-            <p>{character.aliases[0]}</p>
-          )
-        )
-      }
+      <button onClick={alphabeticOrder}>Alphabetic Order</button>
+      <button onClick={bookOrder}>Book Order</button>
+      {renderList}
     </div>
   );
 }
