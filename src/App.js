c09b7f564c2e19deb282d1c42bd8570b1ed5cbbd
Henrique Fadoni <henrique@feltapp.com>
2021-01-14 15:42:56 -0300

    Adding Style & Organize

--- src/App.js
+++ src/App.js
@@ -15,35 +15,46 @@ function App() {
   }, []);
 
   const alphabeticOrder = () => {
-    const newCharacters = characters.sort((charA, charB) => {
+    const newList = characters.sort((charA, charB) => {
       if (charA.aliases < charB.aliases) return -1
       if (charA.aliases > charB.aliases) return 1
       return 0;
     });
 
-    setCharacters([...newCharacters]);
+    setCharacters([...newList]);
   }
 
   const bookOrder = () => {
-    const newCharacters = characters.sort((charA, charB) => {
-      if (charA.books[0] < charB.books[0]) return -1
+    const newList = characters.sort((charA, charB) => {
+      if (charA.books[0] > charB.books[0]) return -1
       if (charA.books[0] > charB.books[0]) return 1
       return 0;
     });
 
-    setCharacters([...newCharacters]);
+    setCharacters([...newList]);
   }
 
-  let renderList = characters.map((character, i) => (
-      <p key={i}>{character.aliases[0]}</p>
-    )
-  )
+  const renderList = characters.map((character, i) => (
+    <p key={i}>{character.aliases[0]}</p>
+  ));
 
   return (
     <div className="App">
-      <h1>Find Your Favorite Game of Thrones Characters</h1>
-      <button onClick={alphabeticOrder}>Alphabetic Order</button>
-      <button onClick={bookOrder}>Book Order</button>
+      <h1>
+        Find Your Favorite Game of Thrones Characters
+      </h1>
+      <button onClick={alphabeticOrder}>
+        Alphabetic Order
+      </button>
+      <button
+        onClick={bookOrder}
+        style={{
+          marginLeft: 10,
+          cursor: 'pointer'
+        }}
+      >
+        Book Order
+      </button>
       {renderList}
     </div>
   );
